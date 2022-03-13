48fcf4d05c32ef333fd8afd361f077e89ea02d6b
rickkky <rickkky@foxmail.com>
2022-03-13 16:39:17 +0800

    feat(p1, ts): hash table solution

---
+++ solutions/1.two-sum/hash-table.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * https://leetcode-cn.com/problems/two-sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  const map: Map<number, number> = new Map();
+
+  for (const [i, num] of nums.entries()) {
+    const diff = target - num;
+
+    if (map.has(diff)) {
+      return [map.get(diff), i];
+    }
+
+    map.set(num, i);
+  }
+
+  return [];
+}
+// @lc code=end
