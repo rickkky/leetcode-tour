784f8908e85a031e480387e5ef6e3dc78f53537d
rickkky <rickkky@foxmail.com>
2022-03-15 00:51:21 +0800

    feat(p9, rust): iteration solution

---
+++ solutions/9.palindrome-numbe/iteration.rs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=rust
+ *
+ * https://leetcode-cn.com/problems/palindrome-numbe
+ */
+
+// @lc code=start
+impl Solution {
+  pub fn is_palindrome(x: i32) -> bool {
+    if x < 0 {
+      return false;
+    }
+
+    let mut reversed = 0;
+    let mut quotient = x;
+
+    while quotient > 0 {
+      reversed = reversed * 10 + quotient  % 10;
+      quotient /= 10;
+    }
+
+    reversed == x
+  }
+}
+// @lc code=end
+
