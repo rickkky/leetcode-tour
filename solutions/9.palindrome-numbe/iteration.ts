5815142e616b470cbd9806afe57ea9b05782fefe
rickkky <rickkky@foxmail.com>
2022-03-15 00:50:48 +0800

    feat(p9, ts): iteration solution

---
+++ solutions/9.palindrome-numbe/iteration.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=typescript
+ *
+ * https://leetcode-cn.com/problems/palindrome-numbe
+ */
+
+// @lc code=start
+function isPalindrome(x: number): boolean {
+  if (x < 0) {
+    return false;
+  }
+
+  let reversed = 0;
+  let quotient = x;
+
+  while (quotient > 0) {
+    reversed = reversed * 10 + (quotient % 10);
+    quotient = Math.floor(quotient / 10);
+  }
+
+  return reversed === x;
+}
+// @lc code=end
