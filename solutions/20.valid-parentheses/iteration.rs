ae0a687d8f8f22b2ccee733ca536242adc30c279
rickkky <rickkky@foxmail.com>
2022-03-16 00:10:02 +0800

    feat(p20, rust): iteration solution

---
+++ solutions/20.valid-parentheses/iteration.rs
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=rust
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses
+ */
+
+// @lc code=start
+impl Solution {
+  pub fn is_valid(s: String) -> bool {
+    let mut stack: Vec<char> = Vec::new();
+    for c in s.chars() {
+      match c {
+        '(' | '[' | '{' => {
+          stack.push(c);
+        }
+        ')' => {
+          if stack.pop() != Some('(') {
+            return false;
+          }
+        }
+        ']' => {
+          if stack.pop() != Some('[') {
+            return false;
+          }
+        }
+        '}' => {
+          if stack.pop() != Some('{') {
+            return false;
+          }
+        }
+        _ => {}
+      }
+    }
+    stack.is_empty()
+  }
+}
+// @lc code=end
+
