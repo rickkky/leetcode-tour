514c77f3c9e4e793005f28737bcfcd4b9a2961d3
rickkky <rickkky@foxmail.com>
2022-03-17 08:53:52 +0800

    feat(p28, rust): iteration solution

---
+++ solutions/28.implement-strstr/iteration.rs
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=rust
+ *
+ * https://leetcode-cn.com/problems/implement-strstr
+ */
+
+// @lc code=start
+impl Solution {
+  pub fn str_str(haystack: String, needle: String) -> i32 {
+    if needle.is_empty() {
+      return 0;
+    }
+    let mut i = 0;
+    let mut j = 0;
+    while i < haystack.len() {
+      if haystack.as_bytes()[i] == needle.as_bytes()[j] {
+        i += 1;
+        j += 1;
+      } else {
+        i = i - j + 1;
+        j = 0;
+      }
+      if j == needle.len() {
+        return (i - j) as i32;
+      }
+    }
+    -1
+  }
+}
+// @lc code=end
+
