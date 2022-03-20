5ef44964c08894e1343bddcd7af53bdb0092b6db
rickkky <rickkky@foxmail.com>
2022-03-20 10:53:07 +0800

    feat(p27, rust): interation, two pointers solutions

---
+++ solutions/27.remove-element/two-pointers.rs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=rust
+ *
+ * https://leetcode-cn.com/problems/remove-element
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len();
+        while left < right {
+            if nums[left] == val {
+                nums[left] = nums[right - 1];
+                right -= 1;
+            } else {
+                left += 1;
+            }
+        }
+        left as i32
+    }
+}
+// @lc code=end
