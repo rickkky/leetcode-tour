f7c28ccc843e3e92b20669a615a38ccfa1ce29b2
rickkky <rickkky@foxmail.com>
2022-04-03 11:11:34 +0800

    feat(p141, ts): tow pointers solution

---
+++ solutions/141.linked-list-cycle/tow-pointers.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=typescript
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle
+ */
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+// @lc code=start
+function hasCycle(head: ListNode | null): boolean {
+    if (head === null) {
+        return false;
+    }
+    let slow = head;
+    let fast = head;
+    while (fast.next && fast.next.next) {
+        slow = slow.next as ListNode;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+    return false;
+}
+// @lc code=end
+
+export {};
