eac4a844e11c5aa051c6c209e53d77f7e6d059de
rickkky <rickkky@foxmail.com>
2022-04-06 23:16:54 +0800

    feat(p206, ts): iteration solution

---
+++ solutions/206.reverse-linked-list/iteration.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=typescript
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list
+ */
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+// @lc code=start
+function reverseList(head: ListNode | null): ListNode | null {
+    let curr: ListNode | null = head;
+    let prev: ListNode | null = null;
+    while (curr) {
+        const next: ListNode | null = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+// @lc code=end
+
+export {};
