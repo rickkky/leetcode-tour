821f7a846a111ba192aff88b76001fd55095cc59
rickkky <rickkky@foxmail.com>
2024-07-01 23:11:18 +0800

    refactor: update

--- solutions/1.two-sum/src/hash_table.rs
+++ solutions/1.two-sum/src/hash_table.rs
@@ -4,11 +4,11 @@
  * https://leetcode-cn.com/problems/two-sum/
  */
 
-use std::collections::HashMap;
-
 pub struct Solution;
 
 // @lc code=start
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut map = HashMap::new();
