867b68f187607213d1db7b41c5049dc43f5b9c26
rickkky <rickkky@foxmail.com>
2024-07-01 23:13:09 +0800

    feat(p48, rs): hash table

---
+++ solutions/48.group-anagrams/src/hash_table.rs
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=rust
+ *
+ * https://leetcode.cn/problems/group-anagrams/
+ */
+
+pub struct Solution;
+
+// @lc code=start
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut map: HashMap<[i32; 26], Vec<String>> = HashMap::new();
+        for s in strs {
+            let mut count = [0; 26];
+            for c in s.chars() {
+                count[(c as u8 - b'a') as usize] += 1;
+            }
+            map.entry(count).or_insert(vec![]).push(s);
+        }
+        map.into_iter().map(|(_, v)| v).collect()
+    }
+}
+// @lc code=end
