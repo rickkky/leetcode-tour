60e50f650b16a5604194e39acc36da676c79aaa6
rickkky <rickkky@foxmail.com>
2024-07-04 12:46:49 +0800

    feat(p42, rs): monotonic stack

---
+++ solutions/42.trapping-rain-water/src/monotonic_stack.rs
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=rust
+ *
+ * https://leetcode.cn/problems/trapping-rain-water/
+ */
+
+pub struct Solution;
+
+// @lc code=start
+impl Solution {
+    pub fn trap(heights: Vec<i32>) -> i32 {
+        let mut stack = Vec::new();
+        let mut trapped = 0;
+        for (i, &h) in heights.iter().enumerate() {
+            while let Some(&top) = stack.last() {
+                if h < heights[top] {
+                    break;
+                }
+                stack.pop();
+                if let Some(&prev) = stack.last() {
+                    trapped += (i - prev - 1) as i32 * (h.min(heights[prev]) - heights[top]);
+                }
+            }
+            stack.push(i);
+        }
+        trapped
+    }
+}
+// @lc code=end
