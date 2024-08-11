4e4ae30e380c0b96440c467a54b840e846774dce
rickkky <rickkky@foxmail.com>
2024-08-11 23:30:18 +0800

    feat(p160, ts): hash table, two pointers

---
+++ solutions/160.intersection-of-two-linked-lists/src/hash_table.ts
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=typescript
+ *
+ * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ */
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+// @lc code=start
+function getIntersectionNode(
+    l1: ListNode | null,
+    l2: ListNode | null,
+): ListNode | null {
+    const set = new Set<ListNode>();
+    let p1: ListNode | null = l1;
+    let p2: ListNode | null = l2;
+    while (p1 || p2) {
+        if (p1) {
+            if (set.has(p1)) {
+                return p1;
+            } else {
+                set.add(p1);
+                p1 = p1.next;
+            }
+        }
+        if (p2) {
+            if (set.has(p2)) {
+                return p2;
+            } else {
+                set.add(p2);
+                p2 = p2.next;
+            }
+        }
+    }
+    return null;
+}
+// @lc code=end
+
+export { getIntersectionNode };
