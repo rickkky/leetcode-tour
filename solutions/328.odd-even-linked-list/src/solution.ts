fcaeda40cab4271d0205decea1e72cc1051a7e26
rickkky <rickkky@foxmail.com>
2024-08-11 23:40:08 +0800

    feat(p328, ts): solution

--- solutions/328.odd-even-linked-list/src/solution.ts
+++ solutions/328.odd-even-linked-list/src/solution.ts
@@ -1,12 +1,10 @@
 /*
  * @lc app=leetcode.cn id=328 lang=typescript
  *
- * https://leetcode-cn.com/problems/odd-even-linked-list
+ * https://leetcode.cn/problems/odd-even-linked-list/
  */
 
-/**
- * Definition for singly-linked list.
- */
+// Definition for singly-linked list.
 class ListNode {
     val: number;
     next: ListNode | null;
@@ -21,18 +19,18 @@ function oddEvenList(head: ListNode | null): ListNode | null {
     if (!head) {
         return null;
     }
+    const evenHead = head.next;
     let odd = head;
-    let even = head.next;
-    const evenHead = even;
+    let even = evenHead;
     while (even && even.next) {
         odd.next = even.next;
-        odd = even.next;
+        odd = odd.next;
         even.next = odd.next;
-        even = odd.next;
+        even = even.next;
     }
     odd.next = evenHead;
     return head;
 }
 // @lc code=end
 
-export {};
+export { oddEvenList };
