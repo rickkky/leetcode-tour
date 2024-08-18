b1f677347d88fb93b2de5b3a7dfcab79841573a0
rickkky <rickkky@foxmail.com>
2024-08-18 20:40:58 +0800

    feat(p402, rs): stack

---
+++ solutions/402.remove-k-digits/src/stack.rs
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=402 lang=rust
+ *
+ * https://leetcode-cn.com/problems/remove-k-digits/
+ */
+
+pub struct Solution;
+
+// @lc code=start
+impl Solution {
+    pub fn remove_kdigits(s: String, mut k: i32) -> String {
+        let mut stack = vec![];
+        for c in s.chars() {
+            while k > 0 && !stack.is_empty() && stack.last().unwrap() > &c {
+                stack.pop();
+                k -= 1;
+            }
+            stack.push(c);
+        }
+        while k > 0 {
+            stack.pop();
+            k -= 1;
+        }
+        let mut res = stack
+            .into_iter()
+            .collect::<String>()
+            .trim_start_matches('0')
+            .to_string();
+        if res.is_empty() {
+            res = "0".to_string();
+        }
+        res
+    }
+}
+// @lc code=end
